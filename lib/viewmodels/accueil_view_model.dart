9b5a17b1941db5024577d3f44e21d7b68ba37171
ZouaouiIheb12 <zouaouiiheb12@gmail.com>
2024-11-01 10:35:58 +0100

    Page d'accueil

---
+++ lib/viewmodels/accueil_view_model.dart
@@ -0,0 +1,11 @@
+class AccueilViewModel {
+  void onTryNowPressed() {
+    
+    print("Try Now pressed");
+  }
+
+  void onLoginPressed() {
+    
+    print("Login pressed");
+  }
+}
---
+++ lib/views/page_accueil.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import '../viewmodels/accueil_view_model.dart';
+
+class PageAccueil extends StatelessWidget{
+   final AccueilViewModel viewModel = AccueilViewModel();
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+             RichText(
+  text: TextSpan(
+    children: [
+      TextSpan(
+        text: 'Coac',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+        ),
+      ),
+      TextSpan(
+        text: 'hiny',
+        style: TextStyle(
+          color: Colors.orange,
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2.0,
+        ),
+      ),
+    ],
+  ),
+)
+,
+              SizedBox(height: 20),
+
+              Container(
+                width: 200,
+                height: 200,
+                decoration:const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/silhouette.png'), 
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+
+              const   Text(
+                'Welcome',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+            const  Text(
+                'Train and live the new experience',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  viewModel.onTryNowPressed();
+                },
+                style: ElevatedButton.styleFrom(
+                  iconColor: Colors.blue,
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                child: const Text('Try Now'),
+                ),
+                SizedBox(height: 10),
+
+                TextButton(
+                onPressed: () {
+                  viewModel.onLoginPressed();
+                },
+                child: const Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+
+
+
+            ],
+          ),
+        ),
+        ),
+    );
+  }
+}
